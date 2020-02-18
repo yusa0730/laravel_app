bc8490675dcb93424e935b1254876260293bf24f
yusa0730 <isizawa5226@gmail.com>
2020-02-19 02:05:17 +0900

    論理削除機能を追加

--- app/Todo.php
+++ app/Todo.php
@@ -3,15 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 // この Model を継承したfileというのが DB への操作を可能にするfileとなります。
 class Todo extends Model
 {
+    use SoftDeletes;
+
     // $fillable（代入可能）に配列を設定した上でprotectedすれば、設定したもののみ書き換えることができます。今回はtitleのみ書き換え可能。
     protected $fillable = [
         'title',
         'user_id'
     ];
+    protected $dates = ['deleted_at'];
 
     // これを書くことで保存することが可能になり、またユーザーに紐づいたデータ取得のための記述
     public function getByUserId($id)
